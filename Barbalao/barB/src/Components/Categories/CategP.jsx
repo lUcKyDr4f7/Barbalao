4cd570360493e85c0ad26b76cb6586540df97ac9
Lucas Pfutzenreuter Carril <84031296+lUcKyDr4f7@users.noreply.github.com>
2025-10-01 15:29:21 -0300

    Update CategP.jsx

--- Barbalao/barB/src/Components/Categories/CategP.jsx
+++ Barbalao/barB/src/Components/Categories/CategP.jsx
@@ -1,4 +1,4 @@
-import '../Css/styles.categP.module.css'
+import styles from '../Css/styles.categP.module.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -13,15 +13,16 @@ import BebidaA from '../../assets/categorias/drinkA.png'
 export default function CategP() {
   return (
       <>
-        <h2 className='titleCateg'>Cardápio</h2>
-        <Swiper className="brandsSwiper"slidesPerView={2.426} slidesPerGroup={2.426} centerInsufficientSlides={true} allowTouchMove={false} navigation={true} modules={[Navigation]} swipeable={true} slidesOffsetBefore={15} slidesOffsetAfter={395} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
-            <SwiperSlide className="swiper-slide avaliable start"><img src={Lanche}/><h2>Lanches</h2></SwiperSlide>
-            <SwiperSlide className="swiper-slide avaliable"><img src={Pastel}/><h2>Pastéis</h2></SwiperSlide>
-            <SwiperSlide className="swiper-slide avaliable"><img src={Salgado}/><h2>Salgados</h2></SwiperSlide>
-            <SwiperSlide className="swiper-slide avaliable"><img src={Porção}/><h2>Porções</h2></SwiperSlide>
-            <SwiperSlide className="swiper-slide avaliable"><img src={Bebida}/><h2>Bebidas</h2></SwiperSlide>
-            <SwiperSlide className="swiper-slide avaliable end"><img className='bebidaA' src={BebidaA}/><h2 className='bebidA'>Bebidas Alcólicas</h2></SwiperSlide>
+        <h2 className={styles.titleCateg}>Cardápio</h2>
+        <Swiper className={styles.brandsSwiper} slidesPerView={2.426} slidesPerGroup={2.426} centerInsufficientSlides={true} allowTouchMove={false} navigation={true} modules={[Navigation]} swipeable={true} slidesOffsetBefore={15} slidesOffsetAfter={395} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable} ${styles.start}`}><img src={Lanche}/><h2>Lanches</h2></SwiperSlide>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable}`}><img src={Pastel}/><h2>Pastéis</h2></SwiperSlide>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable}`}><img src={Salgado}/><h2>Salgados</h2></SwiperSlide>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable}`}><img src={Porção}/><h2>Porções</h2></SwiperSlide>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable}`}><img src={Bebida}/><h2>Bebidas</h2></SwiperSlide>
+            <SwiperSlide className={`${styles.swiperSlide} ${styles.avaliable} ${styles.end}`}><img className='bebidaA' src={BebidaA}/><h2 className='bebidA'>Bebidas Alcólicas</h2></SwiperSlide>
         </Swiper>
       </>
   );
 }
+
