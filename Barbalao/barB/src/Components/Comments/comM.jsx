98359d779f279bd9e307c0682ed118c53924374a
Zéias <oseias.augusto@aluno.ifsp.edu.br>
2025-10-13 16:40:03 -0300

    Atualização comment

--- Barbalao/barB/src/Components/Comments/comM.jsx
+++ Barbalao/barB/src/Components/Comments/comM.jsx
@@ -8,10 +8,24 @@ export default function ComM(){
     )
 }
 
-""13/10
-- Oséias
-Início do CRUD.
-Criação da pasta e componente para formulário de criação.
-Modificação da Navbar para adicionar o "link" do formulário.
-Adição do useState no App, afim de controlar a aparição do formulário e a array de produtos.
-Modificação do ProdS, afim de pegar as info do form e colocar na estrutura. Isso foi feito por meio do card.""
+// - - - - - - - - - - - - 
+
+// 13/10
+// - Oséias
+// Início do CRUD.
+
+// Criação da pasta e componente para 
+// formulário de criação.
+
+// Modificação da Navbar para adicionar o 
+// "link" do formulário.
+
+// Adição do useState no App, afim de 
+// controlar a aparição do formulário e a 
+// array de produtos.
+
+// Modificação do ProdS, afim de pegar as 
+// info do form e colocar na estrutura. 
+// Isso foi feito por meio do card.
+
+// - - - - - - - - - - - - 
