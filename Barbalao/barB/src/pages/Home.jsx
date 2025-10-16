b07713cfb2d3c9a89445a8a4d6774efebf019334
peziiim <eduardoorx@gmail.com>
2025-10-16 00:03:29 -0300

    feat: Paginas Home e Login

--- Barbalao/barB/src/pages/Home.jsx
+++ Barbalao/barB/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import NavB from '../Components/NavBar/navB.jsx'
 import CategP from '../Components/Categories/CategP.jsx'
 import BannerS from '../Components/Banners/bannerS.jsx'
 import PSection from '../Components/PSection/prodS.jsx'
-import Form from '../Components/Form/Form.jsx'
+import Form from '../Components/Form/FormLogin.jsx'
 
 export default function Home() {
 
--- Barbalao/barB/src/pages/Login.jsx
+++ Barbalao/barB/src/pages/Login.jsx
@@ -1,13 +1,7 @@
-import Form from "../Components/Form/Form"
-import NavB from "../Components/NavBar/navB"
+import Container from "../Components/Form/FormCont"
 
 export default function Login(){
     return <>
-          <NavB/>
-          <Form inputsRange="2" 
-            title="Login" 
-            btn="Logar" 
-            types={["text", "password"]} 
-            placeholders={["Nome", "Senha"]} />
+      <Container/>
     </>
 }
