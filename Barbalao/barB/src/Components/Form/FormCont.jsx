51d5b7e43c901c5e24beba5f66c1f403d4cb7a6c
peziiim <eduardoorx@gmail.com>
2025-10-16 00:02:22 -0300

    feat: Form e FormCont

---
+++ Barbalao/barB/src/Components/Form/FormCont.jsx
@@ -0,0 +1,54 @@
+import Form from "./FormLogin"
+import style from '../Css/styles.formContQ.module.css'
+import { useState } from "react";
+import axios from "axios";
+
+export default function Container(){
+    const [formValues, setFormValues] = useState(["", ""]);
+
+    function handleInputChange (index, value){
+        const newValues = [...formValues];
+        newValues[index] = value;
+        setFormValues(newValues);
+    };
+
+
+     async function handleSubmit(){
+        console.log(formValues); 
+        try {
+            const response = await axios.post('http://localhost:3001/api/login', {
+                nome: formValues[0],
+                senha: formValues[1]
+            })
+
+            console.log("Resposta do servidor: ", response.data)
+        } catch (error){
+            console.log("Erro ao buscar dados: ", error)
+        }
+
+        
+    }
+    
+    return<>
+        <nav className={style.formNav}>
+          <a className={style.formLink} href="/">início</a>
+          <a className={style.formLink} href="/about-us">Sobre nós</a>
+        </nav>
+            <div className={style.cont}>
+                <div className={style.textCont}>
+                    <h1 className={style.contTitle}>Bem Vindo!</h1>
+                    <h4 className={style.contSubtitle}>Quais mudanças serão feitas hoje?</h4>
+                </div>
+                 <Form inputsRange="2" 
+                        title="Login" 
+                        btn="Logar" 
+                        types={["text", "password"]} 
+                        placeholders={["Nome", "Senha"]} 
+                        aLink='/'
+                        values={formValues}
+                        onInputChange={handleInputChange}
+                        click={handleSubmit}
+                        />
+            </div>
+    </>
+}
