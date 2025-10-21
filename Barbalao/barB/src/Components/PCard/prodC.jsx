18bc66bda86784092689c498ed7638f6a9704c76
Lucas Pfutzenreuter Carril <84031296+lUcKyDr4f7@users.noreply.github.com>
2025-10-21 00:10:28 +0000

    CardC-update

--- Barbalao/barB/src/Components/PCard/prodC.jsx
+++ Barbalao/barB/src/Components/PCard/prodC.jsx
@@ -2,37 +2,40 @@ import styles from '../Css/styles.pcard_C.module.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useState } from 'react';
 import { Carousel } from "react-responsive-carousel";
+import Hamburgao from '../../assets/popular-imgs/hamburgao.png';
 export default function ProdC() {
     const [quantity, setQuantity] = useState(1);
-
     const increase = () => {
-        if(quantity > 11){
             setQuantity(quantity + 1);
-        }
     }
     const decrease = () => {
-        if(quantity > 1){
             setQuantity(quantity - 1);
-        }
     }
     return (
          <>
-         <section className=''>
-            <div className=''>
-                <div style={{ width: "100%", margin: "0 auto" }}>
-                <Carousel className={''} showThumbs={true} swipeable={true}>
-                    <div><img src="https://i.ibb.co/4f1y2vY/prod-C1.jpg" alt="Slide 1" /></div>
-                    <div><img src="https://i.ibb.co/7G6v3vD/prod-C2.jpg" alt="Slide 2" /></div>
+         <section className={styles.screenBlur}>
+            <div className={styles.productCard}>
+                <div >
+                <Carousel className={styles.carousel} showArrows={true} showThumbs={true} showStatus={false} swipeable={true} showIndicators={false}>
+                    <div className={styles.imagemC}>
+                        <img src={Hamburgao}/>
+                        <p>1</p>
+                    </div>
+                    <div className={styles.imagemC}>
+                        <img src={Hamburgao}/>
+                        <p>2</p>
+                    </div>
                 </Carousel>
                 </div>
                 <div>
                     <h1>Title</h1>
                     <p>Description</p>
-                    <div class="product-buy">
-                        <div class="product-quantity"><button class="decrease-btn" onClick={decrease}>-</button><span class="quantity">{quantity}</span><button class="increase-btn" onClick={increase}>+</button></div>
+                    <div class={styles.productBuy}>
+                        <div class="product-quantity"><button class="decrease-btn" onClick={quantity > 1 && decrease}>-</button><span class="quantity">{quantity}</span><button class="increase-btn" onClick={quantity < 10 && increase}>+</button></div>
                         <div class="add-to-cart-btn border-btn">Adicionar ao carrinho <i class="ri-shopping-cart-2-fill"></i></div>
                     </div>
                 </div>
+                <a className={styles.closeBtn}><i class="ri-close-line"></i></a>
             </div>
          </section>
         </>
