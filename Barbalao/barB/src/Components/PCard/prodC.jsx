2c9aa00b57ad483ef91acf5d2843a2c9d860734f
Lucas Pfutzenreuter Carril <84031296+lUcKyDr4f7@users.noreply.github.com>
2025-10-21 22:37:44 +0000

    ProdC Update

--- Barbalao/barB/src/Components/PCard/prodC.jsx
+++ Barbalao/barB/src/Components/PCard/prodC.jsx
@@ -1,10 +1,28 @@
 import styles from '../Css/styles.pcard_C.module.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Carousel } from "react-responsive-carousel";
 import Hamburgao from '../../assets/popular-imgs/hamburgao.png';
 export default function ProdC() {
     const [quantity, setQuantity] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+    useEffect(() => {
+    if (isOpen) {
+        const scrollY = window.scrollY;
+        document.body.style.top = `-${scrollY}px`;
+        document.body.classList.add('lock-scroll');
+    } else {
+        const top = document.body.style.top;
+        document.body.classList.remove('lock-scroll');
+        document.body.style.top = '';
+        // restaura a posição de rolagem
+        const y = top ? parseInt(top) * -1 : 0;
+        window.scrollTo(0, y);
+    }
+    }, [isOpen]);
+
+
+
     const increase = () => {
             setQuantity(quantity + 1);
     }
@@ -13,31 +31,32 @@ export default function ProdC() {
     }
     return (
          <>
+         {isOpen && (
          <section className={styles.screenBlur}>
             <div className={styles.productCard}>
                 <div >
-                <Carousel className={styles.carousel} showArrows={true} showThumbs={true} showStatus={false} swipeable={true} showIndicators={false}>
+                <Carousel className={styles.carousel} showArrows={false} showThumbs={true} showStatus={false} swipeable={true} showIndicators={false}>
                     <div className={styles.imagemC}>
                         <img src={Hamburgao}/>
-                        <p>1</p>
                     </div>
                     <div className={styles.imagemC}>
                         <img src={Hamburgao}/>
-                        <p>2</p>
                     </div>
                 </Carousel>
                 </div>
-                <div>
-                    <h1>Title</h1>
-                    <p>Description</p>
+                <div className={styles.productInfo}>
+                    <h1>Hamburgão</h1>
+                    <p className={styles.description}>Suculento hambúrguer artesanal preparado com pão brioche macio, blend de carne bovina 180g grelhado no ponto perfeito, coberto por queijo cheddar derretido, alface crocante, tomate fresco e molho especial da casa. Finalizado com um toque de cebola caramelizada que realça o sabor e transforma cada mordida em uma experiência única</p>
+                    <p className={styles.price}>R$ 29,90</p>
                     <div class={styles.productBuy}>
-                        <div class="product-quantity"><button class="decrease-btn" onClick={quantity > 1 && decrease}>-</button><span class="quantity">{quantity}</span><button class="increase-btn" onClick={quantity < 10 && increase}>+</button></div>
-                        <div class="add-to-cart-btn border-btn">Adicionar ao carrinho <i class="ri-shopping-cart-2-fill"></i></div>
+                        <div class={styles.productQuantity}><button onClick={quantity > 1 && decrease}>-</button><span>{quantity}</span><button onClick={quantity < 10 && increase}>+</button></div>
+                        <div class={styles.addCart}><button onClick={''}>Adicionar ao carrinho <i class="ri-shopping-cart-2-fill"></i></button></div>
                     </div>
                 </div>
-                <a className={styles.closeBtn}><i class="ri-close-line"></i></a>
+                <a className={styles.closeBtn} onClick={() => setIsOpen(false)}><i class="ri-close-line"></i></a>
             </div>
          </section>
+         )}
         </>
     )
 }
