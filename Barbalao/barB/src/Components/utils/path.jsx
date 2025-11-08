9da92700986785c6e9700342da46812dbc180620
Lucas Pfutzenreuter Carril <84031296+lUcKyDr4f7@users.noreply.github.com>
2025-11-08 00:21:21 +0000

    RU

--- Barbalao/barB/src/Components/utils/path.jsx
+++ Barbalao/barB/src/Components/utils/path.jsx
@@ -1,3 +1,3 @@
 export function getImagePath(banner){
-        return (`<div className="" />src/assets/Banners/${banner.imagemId}.jpg`); 
+        return (`/src/assets/Banners/${banner.imagemId}.jpg`); 
 }
--- Barbalao/barB/src/Components/utils/pathP.jsx
+++ Barbalao/barB/src/Components/utils/pathP.jsx
@@ -1,3 +1,3 @@
 export function getImagePath(product){
-        return (`./src/assets/popular-imgs/${product.image}.png`); 
+        return (`/src/assets/popular-imgs/${product.image}.png`); 
 }
