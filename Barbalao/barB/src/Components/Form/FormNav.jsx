c3d80325607befb0345132e9939114edf1bcaffa
peziiim <eduardoorx@gmail.com>
2025-11-09 10:41:04 -0300

    fix

--- Barbalao/barB/src/Components/Form/FormNav.jsx
+++ Barbalao/barB/src/Components/Form/FormNav.jsx
@@ -2,12 +2,12 @@ import { Link, Outlet } from 'react-router-dom';
 import style from '../Css/styles.formNav.module.css'
 import Logo from '../../assets/BarB.png'
 
-export default function FormNav () {
+export default function FormNav() {
     return (
         <>
             <nav className={style.formNav}>
                 <div>
-                    <Link to="/"><img className={style.img} src={Logo} alt="Logo do var balão" /></Link>
+                    <Link to="/"><img className={style.img} src={Logo} alt="Logo do bar balão" /></Link>
                 </div>
                 <div>
                     <Link className={style.formLink} to="/">início</Link>
