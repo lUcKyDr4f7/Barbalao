ff4b470527eadfba8327b179c7c6d33bdcb31982
Leonardo <leoavalente@gmail.com>
2025-11-25 20:57:36 -0300

    Update CategP.jsx
    go to section

--- Barbalao/barB/src/Components/Categories/CategP.jsx
+++ Barbalao/barB/src/Components/Categories/CategP.jsx
@@ -21,7 +21,7 @@ export default function CategP({ categorias }) {
           {categoriasL.length !== 0?
             categoriasL.map((categ, i) => (
               <>
-              <div className={`${styles.swiperCateg}`}>
+              <div className={`${styles.swiperCateg}`} onClick(() => scrollToSection(categ.id_categoria)>
                 <img src={categ.imagem}/>
                 <h2>{(categ.nome).slice(0,1).toUpperCase() + (categ.nome).slice(1)}</h2>
               </div>
@@ -42,3 +42,4 @@ export default function CategP({ categorias }) {
   );
 
 }
+
