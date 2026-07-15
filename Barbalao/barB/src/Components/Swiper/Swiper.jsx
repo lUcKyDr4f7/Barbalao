4dce953b29f3ae41853c0773fbf1d23afa8f489a
Leonardo <leoavalente@gmail.com>
2026-07-15 13:49:14 +0000

    Swiper verifica começo e fim

--- Barbalao/barB/src/Components/Swiper/Swiper.jsx
+++ Barbalao/barB/src/Components/Swiper/Swiper.jsx
@@ -1,27 +1,45 @@
-import styles from '../Css/styles.swiper.module.css';
-import { useRef } from 'react';
+import styles from './styles.swiper.module.css';
+import { useState, useRef, useEffect } from 'react';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
 
 export default function Swiper(props) {
   
   const swiperRef = useRef(null)
+  const [isSwiperStart, setIsSwiperStart] = useState(false);
+  const [isSwiperEnd, setIsSwiperEnd] = useState(false);
 
-  function swipeLeft() {
-    let gap = Number(getComputedStyle(swiperRef.current).getPropertyValue("column-gap").slice(0, -2));
-    swiperRef.current.scrollBy({top: 0, left: -(9 * window.innerWidth / 10 + gap), behavior: "smooth",});
-    console.log();
+  function checkSwiperSection(swiper, scrollDist) {
+    let numElements = props.children.length;
+    if(swiper.offsetWidth >= swiper.scrollWidth) {
+      setIsSwiperStart(true);
+      setIsSwiperEnd(true);
+      return;
+    }
+    setIsSwiperStart((swiper.scrollLeft + scrollDist) <= 0 ? true : false);
+    setIsSwiperEnd((swiper.scrollLeft + 2*scrollDist) >= swiper.scrollWidth ? true : false);
   }
 
-  function swipeRight() {
+  function swipe(direction) {
     let gap = Number(getComputedStyle(swiperRef.current).getPropertyValue("column-gap").slice(0, -2));
-    swiperRef.current.scrollBy({top: 0, left: 9 * window.innerWidth / 10 + gap, behavior: "smooth",});
+    let scrollDist = direction * (swiperRef.current.offsetWidth + gap);
+    swiperRef.current.scrollBy({top: 0, left: scrollDist, behavior: "smooth",});
+    checkSwiperSection(swiperRef.current, scrollDist);
   }
+
+  useEffect(() => {
+    checkSwiperSection(swiperRef.current, 0);
+  }, [])
 
   return (
       <>
         <div className={styles.swiper}>
-          <button className={styles.swipeBtn} onClick={() => swipeLeft()}><i className="ri-arrow-left-s-line"></i></button>
+          <button className={styles.swipeBtn} onClick={() => !isSwiperStart && swipe(-1)}>
+            {!isSwiperStart && <ChevronLeft />}
+          </button>
           <div key='swiper' className={styles.swiperSlide} ref={swiperRef}>{props.children}</div>
-          <button className={styles.swipeBtn} onClick={() => swipeRight()}><i className="ri-arrow-right-s-line"></i></button>
+          <button className={styles.swipeBtn} onClick={() => !isSwiperEnd && swipe(1)}>
+            {!isSwiperEnd && <ChevronRight />}
+          </button>
         </div>
       </>
   );
